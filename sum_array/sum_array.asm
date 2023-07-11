e0e41483ef4c2c5d3a31c30fe80a3a8e9839498c
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-07-11 14:39:09 +0700

    add sum array

--- sum_array/sum_array.asm
+++ sum_array/sum_array.asm
@@ -1,31 +1,56 @@
 .model tiny
 .stack 100h
 .data
-    tb1 db 'Enter first number : $'
-    tb2 db 13, 10, 'Enter second number : $'
-    tb3 db 13, 10, 'Sum two numbers is : $'
-    number1 db 0
-    number2 db 0
+    tb1 db 'Enter number of array : $'
+    tb2 db 13, 10, 'Enter number : $'
+    tb3 db 13, 10, 'Sum numbers of array is : $'
+    number db 0
+    number_of_array db 0
+    sum db 0
 .code
     ;===================== Main =====================;
     main proc
         call load_data
-        call display_notification_1
-        call read_number_1
-        call display_notification_2
-        call read_number_2
-        call display_notification_3
-        call calulation_sum
+        call solve
         call display_result
         call exit_program
     main endp
     
-    ;===================== Read data =====================;
+    ;===================== Load data =====================;
     load_data proc
         mov ax, @data
         mov ds, ax
         ret
     load_data endp
+
+    ;===================== Solve=====================;
+    solve proc
+        call display_notification_1
+        mov number, 0
+        call read_number
+        mov al, number
+        mov number_of_array, al
+
+        read_element:
+            cmp number_of_array, 0
+                je setup_display_sum
+            sub number_of_array, 1
+
+            ;=== read element and add to sum ===
+            call display_notification_2
+            mov number, 0
+            call read_number
+            mov al, sum
+            add al, number
+            mov sum, al
+
+            jmp read_element
+
+        setup_display_sum:
+            call display_notification_3
+            mov al, sum
+            ret
+    solve endp
 
     ;============== Display notification 1 ================;
     display_notification_1 proc
@@ -51,52 +76,27 @@
         ret
     display_notification_3 endp
 
-    ;===================== Read number 2 =====================;
-    read_number_1 proc
+    
+    
+    read_number proc
         mov ah, 1
         int 21h
         cmp al, 13
-        je read_number_1_exit
+        je read_number_exit
         sub al, 30h
         mov cl, al
-        mov al, number1
+        mov al, number
         mov bl, 10
         mul bl
         add al, cl
-        mov number1, al
-        jmp read_number_1
+        mov number, al
+        jmp read_number
     
-    read_number_1_exit:
+    read_number_exit:
         ret
-    read_number_1 endp
+    read_number endp
 
-    ;===================== Read number 2 =====================;
-    read_number_2 proc
-        mov ah, 1
-        int 21h
-        cmp al, 13
-        je read_number_2_exit
-        sub al, 30h
-        mov cl, al
-        mov al, number2
-        mov bl, 10
-        mul bl
-        add al, cl
-        mov number2, al
-        jmp read_number_2
-    
-    read_number_2_exit:
-        ret
-    read_number_2 endp
-
-    ;=====================cal sum=====================;
-    calulation_sum proc
-        mov al, number1
-        add al, number2
-        mov ah, 0
-    calulation_sum endp
-
-    ;=====================Display al=====================;
+    ;=====================Display Result=====================;
     display_result proc
         mov cx, 0
         mov bl, 10
@@ -108,7 +108,6 @@
             cmp al, 0
             je display
             jmp sum_to_stack
-
         display:
             pop ax
             mov dl, ah
